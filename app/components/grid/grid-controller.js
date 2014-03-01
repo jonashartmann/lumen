b7b56c3e9eb3c2773d855a6ec6be9a67c0fce528
Jonas Hartmann <jonasharty@gmail.com>
2014-03-01 19:36:42 +0100

    Render grid by creating layer and rectangles on controller

--- app/components/grid/grid-controller.js
+++ app/components/grid/grid-controller.js
@@ -4,18 +4,32 @@
 	.controller('GridCtrl', ['$scope', 'Grid', function GridCtrl ($scope, Grid) {
 		Grid.init(6, 6);
 		$scope.grid = Grid;
-		$scope.$on('KINETIC:STAGE', function onStageCreated (event, stage) {
-			var rectWidth = Math.floor(stage.getWidth() / Grid.cols);
-			var rectHeight = Math.floor(stage.getHeight() / Grid.rows);
+
+		// Create grid UI after the stage is ready
+		$scope.$on('KINETIC:READY', function onStageCreated (event, stage) {
+			var rectWidth = Math.floor(stage.getWidth() / Grid.cols) - Grid.cols/2;
+			var rectHeight = Math.floor(stage.getHeight() / Grid.rows) - Grid.rows/2;
 			console.log(rectWidth, rectHeight);
-			$scope.opt = {
-				width: rectWidth,
-				height: rectHeight,
-				fill: '#00D2FF',
-				stroke: 'black',
-				strokeWidth: 1,
-				draggable:true
-			};
+			var layer = new Kinetic.Layer();
+
+			for (var i = 0; i < Grid.cols; i++) {
+				for (var j = 0; j < Grid.rows; j++) {
+					var options = {
+						x: (i * rectWidth) + (i*4),
+						y: (j * rectHeight) + (j*4),
+						width: rectWidth,
+						height: rectHeight,
+						fill: '#00D2FF',
+						// stroke: 'black',
+						draggable: false
+					};
+					var rect = new Kinetic.Rect(options);
+					layer.add(rect);
+					// console.log('Added Rectangle', rect);
+				}
+			}
+
+			stage.add(layer);
 		});
 	}]);
 })();
--- app/components/kinetic/stage-directive.js
+++ app/components/kinetic/stage-directive.js
@@ -1,28 +1,35 @@
 (function() {
 	'use strict';
 	angular.module('kinetic')
-	.directive('stage', function stageDirective () {
+	.directive('stage', ['$rootScope', function stageDirective ($rootScope) {
 		return {
 			restrict: 'EA',
+			scope: {
+				stageWidth: '=',
+				stageHeight: '='
+			},
 			link: function linkFn (scope, elem, attrs) {
 				var id = attrs["id"];
 				if (!id) {
 					id = Math.random().toString(36).substring(7);
 					elem.attr('id', id);
 				}
+				var stageWidth = scope.stageWidth || 800;
+				var stageHeight = scope.stageHeight || 600;
+
 				var kinetic = {
 					stage: new Kinetic.Stage({
 						container: id,
-						width: 800,
-						height: 600
+						width: stageWidth,
+						height: stageHeight
 					})
 				};
 
 				scope.kinetic = kinetic;
-				console.log('New Stage Created', kinetic.stage);
+				// console.log('New Stage Created', kinetic.stage);
 
-				scope.$broadcast('KINETIC:STAGE', kinetic.stage);
+				$rootScope.$broadcast('KINETIC:READY', kinetic.stage);
 			}
 		};
-	});
+	}]);
 })();
